f30c5f857f5b7f4f2499e65074f3ed0154fe96da
Jacek Jackiewicz <jjackiewicz86@gmail.com>
2018-06-14 22:49:26 +0200

    Complete task 13.4

--- app/program.js
+++ app/program.js
@@ -4,7 +4,7 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function(){
     var input = process.stdin.read();
     if(input !== null) {
-        var instruction = input.toString().trim();
+        var instruction = input.toString().trim(); 
         switch (instruction){
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -17,7 +17,7 @@ process.stdin.on('readable', function(){
                 process.stdout.write(process.versions.node + '\n');
                 break;
             case '/getOSinfo':
-                getOSinfo();
+            OSinfo.print();
                 break;
             default:
             process.stderr.write('Wrong instruction!\n');       
